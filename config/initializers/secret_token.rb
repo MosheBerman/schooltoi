59e5f5ca28a041407a26e3ac1d927ad5e0784bad
Moshe <moshberm@gmail.com>
2013-09-02 12:37:19 -0400

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schooltoi::Application.config.secret_token = 'b465d765047af11bb80ee1a4f1b9436fa8a3bad30da9e716c7e9c353fdfa65de43d05474b3fd22c6418e636ef6eb272e2baba4f64fce8d8840d542de1beaefe8'
